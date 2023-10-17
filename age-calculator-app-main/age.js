f0855313f470f07c0f8fe4f85df7c8732e9ca2fd
kyaloeric <erickyalo19@gmail.com>
2023-10-17 10:05:26 +0300

    modified UI

--- age-calculator-app-main/age.js
+++ age-calculator-app-main/age.js
@@ -1,50 +1,65 @@
+// get all input elements and error message elements
 let inputs = document.querySelectorAll("input");
 const errorMessage = document.querySelectorAll('.error-message');
 
+// event listener for the first input element
 inputs[0].addEventListener('input', function (event) {
     let value = event.target.value;
-    if (value < 30) {
+    // make sure the value is not less than 1
+    if (value < 1) {
         if (value == 0) {
             value = 1;
         }
         value = value;
-    } else if (value > 30) {
+    }
+    // make sure the value is not greater than 30
+    else if (value > 30) {
         value = 30;
     }
     event.target.value = value;
 });
 
+// event listener for the second input element
 inputs[1].addEventListener('input', function (event) {
     let value = event.target.value;
+    // Ensure the value is not less than 1
     if (value == 0) {
         value = 1;
     }
+    // Ensure the value is not greater than 12
     if (value < 12) {
         value = value;
-    } else if (value > 12) {
+    }
+    else if (value > 12) {
         value = 12;
     }
     event.target.value = value;
 });
 
+// Event listener for the third input element
 inputs[2].addEventListener('input', function (event) {
     let value = event.target.value;
+    // Ensure the value is not less than 1
     if (value == 0) {
         value = 1;
     }
+    // Ensure the value is not greater than the current year
     if (value < 2022) {
         value = value;
-    } else if (value > year) {
+    }
+    else if (value > year) {
         value = year;
     }
     event.target.value = value;
 });
 
+// get the current date
 const now = new Date();
 let year = now.getFullYear();
 let month = now.getMonth() + 1;
 let day = now.getDate();
 
+//  calculate age from birthdate
 function calculateAge(birthdate) {
     const now = new Date();
     const birth = new Date(birthdate);
@@ -70,13 +85,16 @@ function calculateAge(birthdate) {
     return age;
 }
 
+// get elements for displaying calculated age
 const years = document.querySelector("#years");
 const months = document.querySelector("#months");
 const days = document.querySelector("#days");
 const button = document.getElementById("button");
 
+// event listener for the button click
 button.addEventListener('click', function () {
     let isEmpty = false;
+    // Check if any input is empty
     inputs.forEach(function (input) {
         if (input.value.trim() === '') {
             isEmpty = true;
@@ -84,12 +102,13 @@ button.addEventListener('click', function () {
             input.style.border = "1px solid red";
         }
     });
+    // If all inputs are filled, calculate and display age
     if (!isEmpty) {
         for (let i = 0; i < inputs.length; i++) {
             inputs[i].nextSibling.nextSibling.innerHTML = "";
             inputs[i].style.border = "1px solid #ececec";
         }
-        const birthdate = `${inputs[2].value}-${inputs[1].value}-${inputs[0].value}'`;
+        const birthdate = `${inputs[2].value}-${inputs[1].value}-${inputs[0].value}`;
         const age = calculateAge(birthdate);
         years.innerHTML = age.years;
         months.innerHTML = age.months;
--- jspracs/todo/todo.js
+++ jspracs/todo/todo.js
@@ -1,58 +1,71 @@
-// Load todos from local storage on page load
-let todos = JSON.parse(localStorage.getItem('todos')) || [];
+      // Load todos from local storage on page load
+      let todos = JSON.parse(localStorage.getItem('todos')) || [];
 
-function saveTodos() {
-    localStorage.setItem('todos', JSON.stringify(todos));
-}
+      function saveTodos() {
+          localStorage.setItem('todos', JSON.stringify(todos));
+      }
 
-function addTodo() {
-    const todoInput = document.getElementById('todoInput');
-    const todoText = todoInput.value.trim();
+      function addTodo() {
+        const todoInput = document.getElementById('todoInput');
+        const todoText = todoInput.value.trim();
 
-    if (todoText !== '') {
-        todos.push({ text: todoText, completed: false });
-        saveTodos();
-        updateTodoList();
-        todoInput.value = '';
+        if (todoText === '') {
+            alert('Please enter a todo before adding.');
+        } else {
+            todos.push({ text: todoText, completed: false });
+            saveTodos();
+            updateTodoList();
+            todoInput.value = '';
+        }
     }
-}
 
-function deleteTodo(index) {
-    todos.splice(index, 1);
-    saveTodos();
-    updateTodoList();
-}
+      function deleteTodo(index) {
+          todos.splice(index, 1);
+          saveTodos();
+          updateTodoList();
+      }
 
-function toggleComplete(index) {
-    todos[index].completed = !todos[index].completed;
-    saveTodos();
-    updateTodoList();
-}
+      function toggleComplete(index) {
+          todos[index].completed = !todos[index].completed;
+          saveTodos();
+          updateTodoList();
+      }
 
-function updateTitle(index) {
-    const newTitle = prompt('Enter a new title:', todos[index].text);
-    if (newTitle !== null) {
-        todos[index].text = newTitle;
-        saveTodos();
-        updateTodoList();
-    }
-}
+      function updateTitle(index) {
+          const newTitle = prompt('Enter a new title:', todos[index].text);
+          if (newTitle !== null) {
+              todos[index].text = newTitle;
+              saveTodos();
+              updateTodoList();
+          }
+      }
 
-function updateTodoList() {
-    const todoList = document.getElementById('todoList');
-    todoList.innerHTML = '';
+      function updateTodoList() {
+          const todoList = document.getElementById('todoList');
+          const completedTasks = document.getElementById('completedTasks');
+          todoList.innerHTML = '';
+          completedTasks.innerHTML = '';
 
-    todos.forEach((todo, index) => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `
-            <input type="checkbox" ${todo.completed ? 'checked' : ''} onchange="toggleComplete(${index})">
-            <span class="${todo.completed ? 'completed' : ''}">${todo.text}</span>
-            <button onclick="deleteTodo(${index})">Delete</button>
-            <button onclick="updateTitle(${index})">Edit</button>
-        `;
-        todoList.appendChild(listItem);
-    });
-}
+          todos.forEach((todo, index) => {
+              const listItem = document.createElement('li');
+              listItem.innerHTML = `
+                  <div class="words">
+                        <span class="${todo.completed ? 'completed' : ''}">${todo.text}</span>
+                  </div>
 
-// Initial rendering
-updateTodoList();
+                  <div class="action-buttons">
+                        <button class="delete" onclick="deleteTodo(${index})">Delete</button>
+                        <button class="edit" onclick="updateTitle(${index})">Edit</button>
+                        <input type="checkbox" ${todo.completed ? 'checked' : ''} onchange="toggleComplete(${index})">
+                  </div>
+              `;
+              if (todo.completed) {
+                  completedTasks.appendChild(listItem);
+              } else {
+                  todoList.appendChild(listItem);
+              }
+          });
+      }
+
+      // Initial rendering
+      updateTodoList();
